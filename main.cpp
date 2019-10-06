213523c8d18c87bd0fbbbc3b4112bf6c69b4a6b0
Daniel Hwang <humble92@gmail.com>
2019-10-06 10:23:15 -0700

    change matrix to vector version

--- main.cpp
+++ main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
+#include <sstream>
 #include "matrix.hpp"
 
+using namespace std;
+
 int main() {
+
+    //constructor test
     matrix m;
+    matrix m1(2);
+    matrix m2(3,4);
+    double a[] = { 10, 20, 30, 40};
+    matrix m3(4, a);
+
+    m3.clear();
+
+    std::stringstream ss;
+    std::string inserted = "This is a sentence.";
+    std::string extracted;
+
+    ss << inserted;
+    ss >> extracted;
+
+    std::cout << inserted;     //  This is a sentence.
+    std::cout << extracted;    //  This
 
     return 0;
 }
--- matrix.hpp
+++ matrix.hpp
@@ -7,39 +7,33 @@
 
 #include <exception>
 #include <cmath>
+#include <vector>
 
 using namespace std;
 
 class matrix {
 private:
-    int height;
-    int width;
-    double** array;
+    int row;
+    int col;
+    vector<vector<double>> data;
 public:
     //default constructor
     matrix() {
-        array = reinterpret_cast<double **>(new double[1][1]);
-        array[0][0] = 0.0;
-        height = width = 1;
+        //vector<double> inner(1, 0.0);
+        data.push_back({0.0});
+        row = col = 1;
     }
 
     //constructor matrix(m) where m is int > 0
     matrix(int m) {
         if(m<=0) {
-            bool ex = true;
-        }
-        try {
+            throw ("out of range error");
+        } else {
             for (int m1=0 ; m1<m ; m1++)
             {
-                for (int m2=0 ; m2<m ; m2++)
-                {
-                    array[m1][m2] = 0.0;
-                }
+                data.push_back(vector<double>(m, 0.0));
+                row = col = m;
             }
-            height = width = m;
-        }
-        catch (bool ex) {
-            throw ("out of range error");
         }
     }
 
@@ -50,66 +44,58 @@ public:
         } else {
             for (int m1=0 ; m1<m ; m1++)
             {
-                for (int m2=0 ; m2<n ; m2++)
-                {
-                    array[m1][m2] = 0.0;
-                }
+                data.push_back(vector<double>(n, 0.0));
             }
-            width = m;
-            height = n;
+            row = m;
+            col = n;
         }
     }
 
-    //constructor matrix(int size, arr[][]) where size is int
-    matrix(int size, double** arr) {
+    //constructor matrix(int size, arr[]) where size is int
+    matrix(int size, double arr[]) {
         if( float(sqrt(size)) != ceil(sqrt(size))) {
             throw ("out of range error");
         } else {
-            for (int m1=0 ; m1<sqrt(size) ; m1++)
+            for (int m1=0 ; m1<size ; m1++)
             {
-                for (int m2=0 ; m2<sqrt(size) ; m2++)
-                {
-                    array[m1][m2] = arr[m1][m2];
-                }
+                data.push_back(vector<double>(arr, arr + size));
             }
+            row = col = size;
         }
     }
 
     //set value
     void set_value(int m, int n, double val) {
-        if( m<0 || n<0 || m >= width ||  n >= height) {
+        if( m<0 || n<0 || m >= row ||  n >= col) {
             throw ("out of range error");
         } else {
-            array[m][n] = val;
+            data[m][n] = val;
         }
     }
 
     //get value
     double get_value(int m, int n) {
-        if( m<0 || n<0 || m >= width ||  n >= height) {
+        if( m<0 || n<0 || m >= row ||  n >= col) {
             throw ("out of range error");
         }
-        return array[m][n];
+        return data[m][n];
     }
 
     //clear to 0.0
     void clear() {
-        for (int m1=0 ; m1<width ; m1++)
+        for (int m1=0 ; m1<row ; m1++)
         {
-            for (int m2=0 ; m2<height ; m2++)
+            for (int m2=0 ; m2<col ; m2++)
             {
-                array[m1][m2] = 0.0;
+                data[m1][m2] = 0.0;
             }
         }
     }
 
-    ~matrix()
-    {
-        for(int i = 0; i < width; ++i) {
-            delete [] array[i];
-        }
-        delete [] array;
-    }
+    // automatically destroyed
+//    ~matrix()
+//    {
+//    }
 };
 
 
